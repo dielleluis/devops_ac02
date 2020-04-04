b4abca12bcb57be49ab5cc440d4fe53c4c0e7297
Luis Dielle <luisdielle@Luiss-MacBook-Pro.local>
2020-04-03 22:49:45 -0300

    first commit

---
+++ com/dielle/operacoes.py
@@ -0,0 +1,14 @@
+class Operacoes():
+
+    def soma(self, valores):
+        val = 0
+        for v in valores:
+            val += v
+        return val
+
+
+    def substracao(self, valores):
+        val = 0
+        for v in valores:
+            val -= v
+        return val
